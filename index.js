4fc7061617fb1875f82fc072fcbf7a0086b80819
rahelAnatasya <rahelanatasya66@gmail.com>
2025-04-11 17:53:29 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import noteRoutes from "./routes/note-route.js";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -8,5 +9,4 @@ app.use(cors());
 app.use(express.json());
 
 app.use(noteRoutes);
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
